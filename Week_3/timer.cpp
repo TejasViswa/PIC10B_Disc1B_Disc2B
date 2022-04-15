257d874849efc3cdedb0b40c74ba309de3b969ef
TejasViswa <45400093+TejasViswa@users.noreply.github.com>
2022-04-14 21:15:52 -0700

    Update timer.cpp

--- Week_3/timer.cpp
+++ Week_3/timer.cpp
@@ -8,8 +8,8 @@ Timer::Timer(unsigned int _hr, unsigned int _min, unsigned int _sec)  : hr(_hr),
 
 std::ostream& operator<<(std::ostream& os, const Timer& t){
     
-    \\ '\0' is a null character, it is similar to "" in this case
-    \\ Here, we check if the variable is a double digit or not. If it are not, then a '0' is placed in front of it.
+    // '\0' is a null character, it is similar to "" in this case
+    // Here, we check if the variable is a double digit or not. If it are not, then a '0' is placed in front of it.
     os << ((t.hr>9)? '\0' : '0') << t.hr << ':'
        << ((t.min>9)? '\0' : '0') << t.min << ':'
        << ((t.sec>9)? '\0' : '0') << t.sec << '\n';   
